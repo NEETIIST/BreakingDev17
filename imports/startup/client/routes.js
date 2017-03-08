78cc080d65d12d0f59529af3ee8155330c583d04
TJacoob <tomas.j.martins@ist.utl.pt>
2017-03-08 19:33:51 +0000

    Bootstrap and SCSS configs

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,9 +2,6 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
-//import '../../ui/layouts/body/body.js';
-//import '../../ui/pages/home/home.js';
-//import '../../ui/pages/not-found/not-found.js';
 import '../../ui/home/home.js'
 
 
