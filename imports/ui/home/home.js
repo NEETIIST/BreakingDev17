54d66ab4d98ca25863ce328f23b8871e570ae372
Tomás Jacob <tomas.j.martins@ist.utl.pt>
2017-06-21 01:54:09 +0100

    Index Page Remade
    Index Page is now set for release

--- imports/ui/home/home.js
+++ imports/ui/home/home.js
@@ -4,5 +4,18 @@ Template.home.events({
 
 	"click #downyougo" : function(){
 		$('html, body').animate({scrollTop: $("#about").offset().top}, 'slow');
+	},
+
+	"click #goLogin" : function(){
+		FlowRouter.go("/login");
 	}
+})
+
+Template.home.helpers({
+	days: function(){
+		var countDownDate = new Date("Sep 22, 2017 15:00:00").getTime();
+		var now = new Date().getTime();
+	  	var distance = countDownDate - now;
+	  	return Math.floor(distance / (1000 * 60 * 60 * 24));
+	},
 })
