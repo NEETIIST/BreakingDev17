a44d076b6b18c5019411a835bd3458349336fb51
Tomás Jacob <tomas.j.martins@ist.utl.pt>
2017-09-04 22:18:07 +0100

    Removed Button Access to Reader

--- imports/ui/code_reader/code_reader.js
+++ imports/ui/code_reader/code_reader.js
@@ -2,23 +2,20 @@ import './code_reader.html';
 
 if (Meteor.isCordova) {
 
-  Template.code_reader.events({
-    'click button': function () {
-
-      cordova.plugins.barcodeScanner.scan(
-        function (result) {
-          alert("We got a barcode\n" +
-            "Result: " + result.text + "\n" +
-            "Format: " + result.format + "\n" +
-            "Cancelled: " + result.cancelled);
-        }, 
-        function (error) {
-          alert("Scanning failed: " + error);
-        }
-     );
-
-    }
-
-  });
+  	Template.code_reader.events({
+		'click button': function () {
+			cordova.plugins.barcodeScanner.scan(
+				function (result) {
+					alert("We got a barcode\n" +
+					"Result: " + result.text + "\n" +
+					"Format: " + result.format + "\n" +
+					"Cancelled: " + result.cancelled);
+				}, 
+				function (error) {
+					alert("Scanning failed: " + error);
+				}
+			);
+		}
+	});
 
 }
